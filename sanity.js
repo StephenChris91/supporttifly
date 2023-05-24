7a85247a94721d51fa9625d9fc131fa560cffc2b
Stephen Chris <46078420+StephenChris91@users.noreply.github.com>
2023-05-24 02:18:30 +0100

    fixed sanity config bug

--- sanity.js
+++ sanity.js
@@ -2,8 +2,8 @@ import { createClient } from 'next-sanity';
 import createImageUrlBuilder from '@sanity/image-url'
 
 export const config = {
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    projectId: 'lt6smir2',
+    dataset: 'production',
     apiVersion: "2023-04-20", // The latest API version at the time of writing
     useCdn: true, // Enable CDN caching
 };
