d4dd3417dc4884067ddc860a35a5be86dd5b8091
Stephen Chris <46078420+StephenChris91@users.noreply.github.com>
2023-07-17 20:53:41 +0100

    updated contents on contact page

--- src/components/Sections/ContactInfoSection.js
+++ src/components/Sections/ContactInfoSection.js
@@ -1,42 +1,41 @@
-import styles from '../../styles/ContactInfo/ContactInfoSection.module.css';
-
+import styles from "../../styles/ContactInfo/ContactInfoSection.module.css";
 
 //icons
-import { BsMailbox2, BsFillPhoneVibrateFill } from 'react-icons/bs'
-import { FaMailBulk } from 'react-icons/fa'
-
-
+import { BsMailbox2, BsFillPhoneVibrateFill } from "react-icons/bs";
+import { FaMailBulk } from "react-icons/fa";
 
 const ContactInfoSection = () => {
-
-
-    return(
-        <div className={styles.contactInfo}>
-            <h1 className={styles.title}>Contact Us</h1>
-            <div className={styles.contactInfoDetailsWrapper}>
-                <div className={`${styles.contactInfoBox} bg-red-100`}>
-                    <BsMailbox2 className={`${styles.icon} ${styles.icon1} text-red-300`}/>
-                    <h2>Address:</h2>
-                    <p>
-                        Ariwola House, Opposite
-                        Ansarudeen School,
-                        Sango-Eleyele Road, Ibadan.
-                    </p>
-                </div>
-                <div className={`${styles.contactInfoBox} bg-green-100`}>
-                    <BsFillPhoneVibrateFill className={`${styles.icon} ${styles.icon2} text-green-300`}/>
-                    <h2>PHONE:</h2>
-                    <p>08054224781</p>
-                </div>
-                <div className={`${styles.contactInfoBox} bg-blue-100`}>
-                    <FaMailBulk className={`${styles.icon} ${styles.icon3} text-blue-300`}/>
-                    <h2>EMAIL:</h2>
-                    <p>08054224781</p>
-                </div>
-            </div>
+  return (
+    <div className={styles.contactInfo}>
+      <h1 className={styles.title}>Contact Us</h1>
+      <div className={styles.contactInfoDetailsWrapper}>
+        <div className={`${styles.contactInfoBox} bg-red-100`}>
+          <BsMailbox2
+            className={`${styles.icon} ${styles.icon1} text-red-300`}
+          />
+          <h2>Address:</h2>
+          <p>
+            Ariwola House, Opposite Ansarudeen School, Sango-Eleyele Road,
+            Ibadan.
+          </p>
         </div>
-    )
-}
+        <div className={`${styles.contactInfoBox} bg-green-100`}>
+          <BsFillPhoneVibrateFill
+            className={`${styles.icon} ${styles.icon2} text-green-300`}
+          />
+          <h2>PHONE:</h2>
+          <p>08054224781</p>
+        </div>
+        <div className={`${styles.contactInfoBox} bg-blue-100`}>
+          <FaMailBulk
+            className={`${styles.icon} ${styles.icon3} text-blue-300`}
+          />
+          <h2>EMAIL:</h2>
+          <p>flysuppng@gmail.com</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-
-export default ContactInfoSection
+export default ContactInfoSection;
